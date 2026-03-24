eb5c015d97aac00e4ba66bd56e85bbe66978b556
Devendra <devendrasinghrajput362@gmail.com>
2026-03-25 00:22:04 +0530

    fix routes

--- app.py
+++ app.py
@@ -1,27 +1,14 @@
 from flask import Flask, render_template
-import pandas as pd
 
 app = Flask(__name__)
 
-# Home page
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "<h1>App is running 🚀</h1>"
 
-# Dashboard page
 @app.route("/dashboard")
 def dashboard():
-    df = pd.read_csv("data.csv")
+    return render_template("dashboard.html")
 
-    total_accidents = len(df)
-    total_injuries = df['Injuries'].sum()
-
-    data = df.to_dict(orient="records")
-
-    return render_template("dashboard.html",
-                           total_accidents=total_accidents,
-                           total_injuries=total_injuries,
-                           data=data)
-# Run app
 if __name__ == "__main__":
     app.run(debug=True)
