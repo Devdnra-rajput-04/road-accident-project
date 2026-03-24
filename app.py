2639475c0fe5f14b8c2713dfee4f6beb4135be90
Devendra <devendrasinghrajput362@gmail.com>
2026-03-25 00:12:24 +0530

    excel style dashboard

--- app.py
+++ app.py
@@ -11,19 +11,17 @@ def home():
 # Dashboard page
 @app.route("/dashboard")
 def dashboard():
-    # CSV file read
     df = pd.read_csv("data.csv")
 
-    # Total accidents = total rows
     total_accidents = len(df)
-
-    # Total injuries (column name same hona chahiye)
     total_injuries = df['Injuries'].sum()
+
+    data = df.to_dict(orient="records")
 
     return render_template("dashboard.html",
                            total_accidents=total_accidents,
-                           total_injuries=total_injuries)
-
+                           total_injuries=total_injuries,
+                           data=data)
 # Run app
 if __name__ == "__main__":
     app.run(debug=True)
